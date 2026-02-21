80303c0d5682340aca103a972c8d2bdde7d023bf
ZhaoxingZhang <zzx19981020@126.com>
2026-02-21 04:50:55 -0800

    chore: 修改script

--- scripts/exp_traffic_v2/exe.sh
+++ scripts/exp_traffic_v2/exe.sh
@@ -5,5 +5,5 @@ nohup sh -c '
   bash scripts/exp_traffic_v2/run_incremental_resnet_gee.sh
   bash scripts/exp_traffic_v2/run_open_set_resnet_baseline.sh
   bash scripts/exp_traffic_v2/run_open_set_resnet_gee.sh
-' > log/exp_traffic_v2.log 2>&1 &
+' > log/exp_traffic_v2_0221.log 2>&1 &
 echo "Dataset 2 experiments started. Check log/exp_traffic_v2.log for progress."
